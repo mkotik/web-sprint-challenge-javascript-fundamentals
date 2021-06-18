d77edb8351a18de7b7e03aefd0bfeb9b0a0a46a0
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-06-18 11:08:17 -0400

    completed stretch, commented it out

--- index.js
+++ index.js
@@ -264,10 +264,25 @@ const cuboidTwo = new CuboidMakerTwo({
 });
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
-console.log(cuboidTwo.volume()); // 100
-console.log(cuboidTwo.surfaceArea()); // 130
+// console.log(cuboidTwo.volume()); // 100
+// console.log(cuboidTwo.surfaceArea()); // 130
 
 // 🦄 💪 Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area. 🦄 💪
+
+class CubeMaker extends CuboidMaker {
+  constructor(attributes) {
+    super(attributes);
+  }
+}
+
+const testCube = new CubeMaker({
+  length: 5,
+  width: 5,
+  height: 5,
+});
+
+console.log(testCube.surfaceArea());
+console.log(testCube.volume());
 
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
 function foo() {
